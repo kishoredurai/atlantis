19760a5ab72b859a6d2890bd6238c06d7db6c72e
kishoredurai <kishoredurai7@gmail.com>
2024-04-09 13:27:16 +0530

    terraform code added

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+## Create an EC2 Instance
+resource "aws_instance" "web" {
+  ami               = "ami-0851b76e8b1bce90b"
+  instance_type     = "t2.medium"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "atlantis-medium"
+  }
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      env     = "dev"
+      role    = "autotagger"
+      verical = "SRE"
+      owner   = "site_reliability_engineering"
+
+    }
+  }
+
+}
